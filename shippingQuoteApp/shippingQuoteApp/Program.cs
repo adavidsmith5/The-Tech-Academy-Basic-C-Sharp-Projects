7210e877972e9619ded82bc6ca7c1648c822aa9f
Tony Smith <46821595+adavidsmith5@users.noreply.github.com>
2019-04-23 09:18:11 -0700

    Create shipping package size requirements and quote
    This app checks the weight and size of a package that the user inputs and then gives a quote if the company would be able to ship it.

---
+++ shippingQuoteApp/shippingQuoteApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shippingQuoteApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("Please enter the weight of your package.");
+            double packageWeight = Convert.ToDouble(Console.ReadLine());
+
+            if (packageWeight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+            }
+            else
+            {
+                Console.WriteLine("What is the package width?");
+                double packageWidth = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("What is the package height?");
+                double packageHeight = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("What is the package length?");
+                double packageLength = Convert.ToDouble(Console.ReadLine());
+
+                double dimensionSum = packageWidth + packageHeight + packageLength;
+
+                if (dimensionSum > 50)
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express.");
+                }
+
+                else
+                {
+                    double quote = dimensionSum * packageWeight / 100;
+                    Console.WriteLine("Your estimate total for shipping this package is: $" + Math.Round(quote,2));
+                }
+
+            }
+            Console.Read();
+        }
+    }
+}
