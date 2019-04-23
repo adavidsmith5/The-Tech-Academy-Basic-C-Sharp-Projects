ab97e874052f3e7740e5d720d7e6f6360c25b4fa
Tony Smith <46821595+adavidsmith5@users.noreply.github.com>
2019-04-23 08:38:53 -0700

    Create insurance qualification app
    Created an app that asks the user a series of questions to find out if they are qualified for automotive insurance.

---
+++ insuranceQualificationApp/insuranceQualificationApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace insuranceQualificationApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            byte age = Convert.ToByte(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI? (Enter true or false)");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            byte speedTickets = Convert.ToByte(Console.ReadLine());
+
+            bool qualified = (age > 15) && (dui == false) && (speedTickets <= 3);
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(qualified);
+
+            Console.Read();
+        }
+    }
+}
