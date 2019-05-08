b4cb0f66821ba56367e9b5e92563780e86a3fc25
Tony Smith <46821595+adavidsmith5@users.noreply.github.com>
2019-05-07 19:57:49 -0700

    Complete challenge
    Added each part of the challenge of iterating through arrays and lists.

--- IterationApp/IterationApp/Program.cs
+++ IterationApp/IterationApp/Program.cs
@@ -10,6 +10,8 @@ namespace IterationApp
     {
         static void Main(string[] args)
         {
+
+            //Take a user's name as input and add it to a set list of strings to make simple sentences
         }
     }
 }
