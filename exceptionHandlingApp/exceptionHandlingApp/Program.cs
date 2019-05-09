ac5d217c40b1d3be2e5bc4b6639ecff7fa02c169
Tony Smith <46821595+adavidsmith5@users.noreply.github.com>
2019-05-09 06:10:53 -0700

    Create exception handling app
    This app has a list of numbers that will be divided by a user input. To deal with the user entering in 0 or a different data type, there are exceptions that can be made to allow the program to continue running.

---
+++ exceptionHandlingApp/exceptionHandlingApp/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exceptionHandlingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numberList = new List<int>();
+            numberList.Add(11);
+            numberList.Add(449);
+            numberList.Add(26);
+            numberList.Add(4);
+            numberList.Add(9);
+            numberList.Add(72);
+            numberList.Add(23);
+
+            try
+            {
+
+
+                Console.WriteLine("Please enter a number to use for division");
+                int divisor = Convert.ToInt32(Console.ReadLine());
+
+                foreach (int num in numberList)
+                {
+                    float quotient = (float)num / (float)divisor;
+                    Console.WriteLine(num + " divided by " + divisor + " equals " + quotient);
+                }
+
+            }
+
+            catch (FormatException e)
+            {
+                Console.WriteLine("Please enter an integer.");
+            }
+
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("You have divided by zero. Please choose another number.");
+            }
+
+            finally
+            {
+                Console.WriteLine("The app has completed, please run again if you did  not get your desired answer.");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
