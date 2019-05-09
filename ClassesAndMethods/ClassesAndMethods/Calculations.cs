65b47a4187f20dab25e7252efbc65ff653138e09
Tony Smith <46821595+adavidsmith5@users.noreply.github.com>
2019-05-09 11:48:51 -0700

    Create example classes and methods app
    This app has one class that has three methods. The methods take an integer input from the user and return an integer after a simple calculation.

---
+++ ClassesAndMethods/ClassesAndMethods/Calculations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethods
+{
+    class Calculations
+    {
+        public static int Add(int num)
+        {
+            int sum = num + 15;
+            return sum;
+        }
+
+        public static int Subtract(int num)
+        {
+            int difference = num - 5;
+            return difference;
+        }
+
+        public static int Multiply(int num)
+        {
+            int product = num * -2;
+            return product;
+        }
+    }
+}
---
+++ ClassesAndMethods/ClassesAndMethods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number for the program to do calculations on.");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int addResult = Calculations.Add(num);
+            Console.WriteLine(addResult);
+
+            int subtractResult = Calculations.Subtract(num);
+            Console.WriteLine(subtractResult);
+
+            int multiplyResult = Calculations.Multiply(num);
+            Console.WriteLine(multiplyResult);
+
+            Console.Read();
+        }
+    }
+}
