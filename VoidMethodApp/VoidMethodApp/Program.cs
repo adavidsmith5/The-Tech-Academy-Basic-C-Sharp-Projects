98e17474b346d53183c09a44479e94d99f1c21e0
Tony Smith <46821595+adavidsmith5@users.noreply.github.com>
2019-05-10 07:12:00 -0700

    Create app with a void method
    The void method takes two parameters, does math with the first and displays the second to the screen.

---
+++ VoidMethodApp/VoidMethodApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VoidMethod method = new VoidMethod();
+            VoidMethod.QuickMath(num1: 5, num2: 6);
+
+            Console.Read();
+        }
+    }
+}
---
+++ VoidMethodApp/VoidMethodApp/VoidMethod.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodApp
+{
+    class VoidMethod
+    {
+        public static void QuickMath(int num1, int num2)
+        {
+            double squareRoot = Math.Sqrt(num1);
+            Console.WriteLine(num2);
+        }
+
+    }
+}
