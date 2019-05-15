0f033da645ff74ef70fd91f030caba9cc86c187e
Tony Smith <46821595+adavidsmith5@users.noreply.github.com>
2019-05-15 09:20:11 -0700

    Add optional parameter class
    Add missedclass

---
+++ OptionalParameterMethod/OptionalParameterMethod/OptionalParameter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameterMethod
+{
+    public class OptionalParameter
+    {
+        public static int ForceOfGravity(int mass, int gravityConstant = 10)
+        {
+
+            int weight = mass * gravityConstant;
+
+            return weight;
+        }
+    }
+}
