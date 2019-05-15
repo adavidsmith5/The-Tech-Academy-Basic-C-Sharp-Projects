d7a292e3c42da423a1b186c38adbfed8ae7ce390
Tony Smith <46821595+adavidsmith5@users.noreply.github.com>
2019-05-15 06:52:44 -0700

    Create App With Overloaded Method
    The method has three possible uses, one with a string entered, one with an integer entered, and one with a decimal entered.

---
+++ OverloadedMethodsApp/OverloadedMethodsApp/Operations.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadedMethodsApp
+{
+    class Operations
+    {
+        public static int Calculation(int num)
+        {
+            int answer = num * -2;
+            return answer;
+        }
+
+        public static int Calculation(double num)
+        {
+            int answer = (int)Math.Ceiling(num);
+            return answer;
+        }
+
+        public static int Calculation(string num)
+        {
+            try
+            {
+                int convertedNum = Convert.ToInt32(num);
+                int answer = convertedNum + 3;
+                return answer;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Please enter an integer value.");
+                return 0;
+            }
+        }
+    }
+}
---
+++ OverloadedMethodsApp/OverloadedMethodsApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadedMethodsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operations operation = new Operations();
+
+            int multiplyByNegTwo = Operations.Calculation(4);
+            Console.WriteLine(multiplyByNegTwo);
+
+            int roundUp = Operations.Calculation(2.98);
+            Console.WriteLine(roundUp);
+
+            int addThree = Operations.Calculation("4");
+            if (addThree != 0)
+            {
+                Console.WriteLine(addThree);
+            }
+            
+
+            Console.Read();
+        }
+    }
+}
