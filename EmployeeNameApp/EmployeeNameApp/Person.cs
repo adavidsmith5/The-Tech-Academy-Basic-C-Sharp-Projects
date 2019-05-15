54a2697101d398b08fb995eeeea0ab9088672d9c
Tony Smith <46821595+adavidsmith5@users.noreply.github.com>
2019-05-15 09:20:57 -0700

    Create app for employee id and name
    App with two classes, name and employee which inherits from name. Then print example out to the console.

---
+++ EmployeeNameApp/EmployeeNameApp/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeNameApp
+{
+    class Person
+    {
+        public void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
