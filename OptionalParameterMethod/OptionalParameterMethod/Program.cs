02389a78078c86f46e3d82f78be5fb29cce73d78
Tony Smith <46821595+adavidsmith5@users.noreply.github.com>
2019-05-15 06:59:51 -0700

    Create app with method that has optional parameter
    This app has a single method that can take two parameters, but one is optional.

---
+++ OptionalParameterMethod/OptionalParameterMethod/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameterMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the mass as an integer");
+            int mass = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the gravitational constant as an integer, or leave blank if on Earth");
+            string gravityResponse = Console.ReadLine();
+
+            int forceOfGravity;
+            if(gravityResponse != "")
+            {
+                int GC = Convert.ToInt32(gravityResponse);
+                forceOfGravity = OptionalParameter.ForceOfGravity(mass, GC);
+            }
+            else
+            {
+                forceOfGravity = OptionalParameter.ForceOfGravity(mass);
+            }
+            Console.WriteLine(forceOfGravity);
+
+            Console.Read();
+
+        }
+    }
+}
