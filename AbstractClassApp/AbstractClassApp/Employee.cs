547fd71d87a710a221601860ca55fe14f8d13243
Tony Smith <46821595+adavidsmith5@users.noreply.github.com>
2019-05-21 07:10:06 -0700

    Overload == operator
    A simple overload of the == operator in the employee class to check if two employees are the same based on their id's.

--- AbstractClassApp/AbstractClassApp/Employee.cs
+++ AbstractClassApp/AbstractClassApp/Employee.cs
@@ -8,6 +8,8 @@ namespace AbstractClassApp
 {
     public class Employee : Person, IQuittable
     {
+        public int id;
+
         public override void SayName()
         {
             base.SayName();
@@ -16,6 +18,30 @@ namespace AbstractClassApp
         public void Quit()
         {
             Console.WriteLine("An employee has quit.");
+        }
+
+        public static bool operator == (Employee employee1, Employee employee2)
+        {
+            if(employee1.id == employee2.id)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator != (Employee employee1, Employee employee2)
+        {
+            if(employee1.id != employee2.id)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
--- AbstractClassApp/AbstractClassApp/Program.cs
+++ AbstractClassApp/AbstractClassApp/Program.cs
@@ -13,12 +13,27 @@ namespace AbstractClassApp
             Employee employee1 = new Employee();
             employee1.firstName = "Sample";
             employee1.lastName = "Student";
+            employee1.id = 1;
 
             employee1.SayName();
 
-            IQuittable employee2 = new Employee();
+            Employee employee2 = new Employee();
+            employee2.firstName = "New";
+            employee2.lastName = "Employee";
+            employee2.id = 1;
 
-            employee2.Quit();
+            if(employee1 == employee2)
+            {
+                Console.WriteLine("These two are the same employees.");
+            }
+            else
+            {
+                Console.WriteLine("These are two different employees.");
+            }
+
+            IQuittable employee = new Employee();
+
+            employee.Quit();
 
             Console.ReadLine();
         }
