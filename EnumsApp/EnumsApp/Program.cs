b7948268a99eb73196a91ca6d892af9a284b2cd0
Tony Smith <46821595+adavidsmith5@users.noreply.github.com>
2019-05-22 07:28:48 -0700

    Create app using enums
    The app has an enum set up for the days of the week. It asks the user for the day, and assigns it to a day of the week if possible. Otherwise it asks the user to enter a correct day of the week.

---
+++ EnumsApp/EnumsApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the day of the week.");
+            string today = Console.ReadLine();
+            today = today.First().ToString().ToUpper() + today.Substring(1);
+            DayOfTheWeek day;
+
+            try
+            {
+                day = (DayOfTheWeek)DayOfTheWeek.Parse(typeof(DayOfTheWeek),today);
+            }
+            catch
+            {
+                Console.WriteLine("Please enter an actual day of the week.(all lowercase or first uppercase only please)");
+            }
+
+            
+
+            Console.Read();
+        }
+    }
+
+    public enum DayOfTheWeek
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+}
