e8d2115195206b7aa76ce5463f8f7a5a1e6b8c7f
Tony Smith <46821595+adavidsmith5@users.noreply.github.com>
2019-05-22 08:35:44 -0700

    Create app with a simple struct
    This app has one struct that is a number, it has a value of Amount that is of decimal value type. It then prints that number to the console.

---
+++ StructExampleApp/StructExampleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructExampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 3.224m;
+            Console.WriteLine(number.Amount);
+            Console.Read();
+        }
+    }
+}
