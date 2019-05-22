eb4f74df76f8cd1a28f9d2746319698fb3a49352
Tony Smith <46821595+adavidsmith5@users.noreply.github.com>
2019-05-22 10:26:08 -0700

    Create app listing employees and searching the list
    This app creates a list of employees and then uses a foreach loop or lambda expression to search the list for a name or id's greater than 5

---
+++ EmployeeLambdaExpressionExamples/EmployeeLambdaExpressionExamples/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeLambdaExpressionExamples
+{
+    public class Employee
+    {
+        public string firstName;
+        public string lastName;
+        public int id;
+    }
+}
---
+++ EmployeeLambdaExpressionExamples/EmployeeLambdaExpressionExamples/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeLambdaExpressionExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>();
+            Employee emp1 = new Employee();
+            emp1.firstName = "Joe";
+            emp1.lastName = "Thomas";
+            emp1.id = 1;
+            employees.Add(emp1);
+
+            Employee emp2 = new Employee();
+            emp2.firstName = "Charles";
+            emp2.lastName = "Jones";
+            emp2.id = 2;
+            employees.Add(emp2);
+
+            Employee emp3 = new Employee();
+            emp3.firstName = "Alice";
+            emp3.lastName = "Williams";
+            emp3.id = 3;
+            employees.Add(emp3);
+
+            Employee emp4 = new Employee();
+            emp4.firstName = "Joe";
+            emp4.lastName = "Baremius";
+            emp4.id = 4;
+            employees.Add(emp4);
+
+            Employee emp5 = new Employee();
+            emp5.firstName = "Carol";
+            emp5.lastName = "Branch";
+            emp5.id = 5;
+            employees.Add(emp5);
+
+            Employee emp6 = new Employee();
+            emp6.firstName = "Karl";
+            emp6.lastName = "Marx";
+            emp6.id = 6;
+            employees.Add(emp6);
+
+            Employee emp7 = new Employee();
+            emp7.firstName = "Barry";
+            emp7.lastName = "Manilow";
+            emp7.id = 7;
+            employees.Add(emp7);
+
+            Employee emp8 = new Employee();
+            emp8.firstName = "Susan";
+            emp8.lastName = "Anthony";
+            emp8.id = 8;
+            employees.Add(emp8);
+
+            Employee emp9 = new Employee();
+            emp9.firstName = "Belvia";
+            emp9.lastName = "Karlsson";
+            emp9.id = 9;
+            employees.Add(emp9);
+
+            Employee emp10 = new Employee();
+            emp10.firstName = "Joe";
+            emp10.lastName = "Little";
+            emp10.id = 10;
+            employees.Add(emp10);
+
+            List<Employee> joes = new List<Employee>();
+
+
+            //foreach(Employee employee in employees)
+            //{
+            //    if(employee.firstName == "Joe")
+            //    {
+            //        joes.Add(employee);
+            //    }
+            //}
+
+
+            //joes = employees.Where(x => x.firstName == "Joe").ToList();
+
+
+            joes = employees.Where(x => x.id > 5).ToList();
+
+            foreach (Employee employee in joes)
+            {
+                Console.WriteLine(employee.lastName);
+            }
+
+
+
+            Console.Read();
+        }
+    }
+}
