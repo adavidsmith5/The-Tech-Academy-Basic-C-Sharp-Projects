d4477547b2ec2fe42695349af7bdb7882e51a8f9
Tony Smith <46821595+adavidsmith5@users.noreply.github.com>
2019-05-27 10:21:41 -0700

    Create app that logs an input number
    The app asks the user for a number, logs that into a text file, and then returns the number to a Console.WriteLine to show the user it worked.

---
+++ LoggingFiles/LoggingFiles/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LoggingFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number.");
+            string num = Console.ReadLine();
+
+            File.WriteAllText(@"C:\Users\6420 i7\Documents\GitHub\The-Tech-Academy-Basic-C-Sharp-Projects\LoggingFiles\logs\log.txt", num);
+
+            string returnNum = File.ReadAllText(@"C:\Users\6420 i7\Documents\GitHub\The-Tech-Academy-Basic-C-Sharp-Projects\LoggingFiles\logs\log.txt");
+            Console.WriteLine(returnNum);
+            Console.Read();
+        }
+    }
+}
