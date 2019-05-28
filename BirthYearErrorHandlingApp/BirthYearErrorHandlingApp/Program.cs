580317630692d6ef8ca859c754f56688b14d3e27
Tony Smith <46821595+adavidsmith5@users.noreply.github.com>
2019-05-28 09:36:55 -0700

    Create app to output user's birth year
    This app will output a user's birth year after they tell their age. The app also handles errors for invalid ages.

---
+++ BirthYearErrorHandlingApp/BirthYearErrorHandlingApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirthYearErrorHandlingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your age.");
+            try
+            {
+                var age = Convert.ToUInt16(Console.ReadLine());
+                var test = 1 / age;
+                var today = DateTime.Today;
+                var birthdate = today.Year - age;
+                Console.WriteLine(birthdate);
+
+                Console.Read();
+            }
+            catch (ArithmeticException)
+            {
+                Console.WriteLine("Please input a positive integer.");
+                Console.Read();
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("Please input a positive integer.");
+                Console.Read();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter a valid integer.");
+                Console.Read();
+            }
+        }
+    }
+}
